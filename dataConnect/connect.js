a3bd098767cbce791ef98fc9cc8e12bfc112f545
Bradley Bruce <bbruce@lcs.com>
2019-06-21 13:48:20 -0400

    Removed credential request due to issues it was having Replaced external sql server ip address with local address

--- dataConnect/connect.js
+++ dataConnect/connect.js
@@ -2,10 +2,10 @@ var sql = require("mssql");
 
 var connect = function () { 
 var conn = new sql.ConnectionPool({ 
-user: '', 
-password: '', 
-server: '', 
-database: ''
+user: 'bruceba', 
+password: 'password', 
+server: '192.168.1.50', 
+database: 'PasswordManager'
  })
 
  return conn; };
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var fs = require('fs')
 var https = require('https')
 var app = express();//create instance of express
 var port = process.env.port || 1337;
-const inquirer = require('inquirer')//for user input
 
 //controllers
 var loginController = require('./dataController/loginController')();
@@ -16,45 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // create application/json parser
 app.use(bodyParser.json());
 
-//get user input
-var questions = [
-    {
-        type: 'input',
-        name: 'serverAddress',
-        message: "Server Address:",
-    },
-    {
-        type: 'input',
-        name: 'database',
-        message: "Database Name:",
-    },
-    {
-        type: 'input',
-        name: 'user',
-        message: "UserName:",
-    },
-    {
-        type: 'input',
-        name: 'password',
-        message: "Password:",
-    },
-]
 
-inquirer.prompt(questions).then(answers => {
-    var serverAddress = (`${answers['serverAddress']}`)
-    var database = (`${answers['database']}`)
-    var user = (`${answers['user']}`)
-    var password = (`${answers['password']}`)
 
-    //write connection file from user input
-    fs.writeFile('./dataConnect/connect.js', "var sql = require(\"mssql\"); \r\n\r\nvar connect = function () { \r\nvar conn = new sql.ConnectionPool({ \r\nuser: '" + user + "', \r\npassword: '" + password + "', \r\nserver: '" + serverAddress + "', \r\ndatabase: '" + database + "'\r\n })\r\n\r\n return conn; };\r\n module.exports = connect;", function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        //console.log("The file was saved!");
-    });
-}).then(function () {
-    
     //after the user input is received, start the server on https
     https = https.createServer({
         key: fs.readFileSync('./dataEncrypt/server.key'),
@@ -64,8 +26,7 @@ inquirer.prompt(questions).then(answers => {
         var message = "Server running on port: " + port + " Started at: " + datetime;
         console.log(message);
 
-    });
-}).then(function () {
+    })
 
     //after the server starts, add controllers
 
@@ -83,5 +44,5 @@ inquirer.prompt(questions).then(answers => {
         response.sendStatus(200);
     });
 
-})
+
    
