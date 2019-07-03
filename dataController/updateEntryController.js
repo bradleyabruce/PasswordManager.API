8811ebcbbee790ba215d461c9764e22a269a3e5f
Bradley Bruce <bruceba@mail.uc.edu>
2019-07-03 11:14:20 -0400

    added validation to update query removed bug that would keep sql connection open for longer than it needed to be Cleaned up code

--- dataController/updateEntryController.js
+++ dataController/updateEntryController.js
@@ -6,7 +6,6 @@ var conn = require("../dataConnect/connect")();
 
 
 var routes = function () {
-
     router.route('/').post(function (req, res) {
 
         var entryid = req.body.entryid;
@@ -23,7 +22,6 @@ var routes = function () {
             var sqlQuery = "SELECT EntryID, WebsiteUsernameID, WebsitePasswordID, CategoryID FROM dbo.tEntries WHERE (EntryID = " + entryid + ")";
 
             var req = new sql.Request(conn);
-
             req.query(sqlQuery).then(function (result) {
 
                 if (result.recordset.length == 0) {
@@ -31,28 +29,26 @@ var routes = function () {
                 }
 
                 else {
-
                     //get values from select query
                     var WebsiteUsernameID = result.recordset[0].WebsiteUsernameID;
                     var WebsitePasswordID = result.recordset[0].WebsitePasswordID;
                     var CategoryID = result.recordset[0].CategoryID;
 
-                    console.log(WebsiteUsernameID + " " + WebsitePasswordID)
-                    newCategoryID++;
-
-
                     //update data in database with new sql call
+                    var sqlQuery = "UPDATE tWebsitePasswords SET WebsitePassword = '" + newPassword + "' WHERE WebsitePasswordID = '" + WebsitePasswordID + "'; UPDATE tWebsiteUsername SET WebsiteUsername = '" + newUsername + "' WHERE WebsiteUsernameID = '" + WebsiteUsernameID + "'; UPDATE tEntries SET CategoryID = '" + newCategoryID + "' WHERE EntryID = '" + entryid + "';";
 
-                        var sqlQuery = "UPDATE tWebsitePasswords SET WebsitePassword = '" + newPassword + "' WHERE WebsitePasswordID = '" + WebsitePasswordID + "'; UPDATE tWebsiteUsername SET WebsiteUsername = '" + newUsername + "' WHERE WebsiteUsernameID = '" + WebsiteUsernameID + "'; UPDATE tEntries SET CategoryID = '" + newCategoryID + "' WHERE EntryID = '';";
+                    var req = new sql.Request(conn);
+                    req.query(sqlQuery).then(function (updateResult) {
+                        
+                        res.json(updateResult.rowsAffected);
+                        conn.close();
 
-                        var req = new sql.Request(conn);
+                    }).catch(function (err) {
+                        conn.close();
+                        res.send("Error: " + err);
+                    });
 
-                        req.query(sqlQuery).then(function (updateResult) {
-
-                            res.json(updateResult.rowsAffected);
-
-                        });
-                }
+                }//end else
 
             }).catch(function (err) {
                 conn.close();
@@ -65,6 +61,7 @@ var routes = function () {
         });
     });
 
+    //conn.close();
     return router;
 };
 module.exports = routes;
